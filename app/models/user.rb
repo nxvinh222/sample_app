e7b3287379bcba7310111850c92e3bc7b87b0584
vinhnx <nxvinh222@gmail.com>
2021-07-29 15:01:04 +0700

    Chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+
+  validates :name, presence: true,
+            length: {
+              maximum: Settings.validate.name.max_length
+            }
+  validates :email, presence: true,
+            length: {
+              minimum: Settings.validate.email.min_length,
+              maximum: Settings.validate.email.max_length
+            },
+            format: {with: Settings.validate.email.format},
+            uniqueness: {case_sensitive: Settings.validate.email.case_sensitive}
+
+  validates :password, presence: true,
+            length: {
+              minimum: Settings.validate.password.min_length
+            }
+
+  has_secure_password
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
